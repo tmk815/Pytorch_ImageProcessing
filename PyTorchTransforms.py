da7c7e8cec85a5f99b93f0fd8f3b92bfa64669f2
門田朋己 <monndenn15@gmail.com>
2020-02-22 20:41:24 +0900

    まとめたクラスを追加

--- PyTorchTransforms.py
+++ PyTorchTransforms.py
@@ -90,3 +90,87 @@ sobel_kernel.state_dict()['bias'].zero_()
 print(sobel_kernel.state_dict().keys())
 print(sobel_kernel.state_dict()['weight'])
 print(sobel_kernel.state_dict()['bias'])
+
+
+# 上記で作成した全カーネルを順番に実行するMyFilter(nn.Module)
+class MyFilter(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 平滑化filter
+        k = 9
+        self.blur_kernel = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.blur_kernel.state_dict()['weight'][0] = torch.FloatTensor([[1 / k, 1 / k, 1 / k],
+                                                                        [1 / k, 1 / k, 1 / k],
+                                                                        [1 / k, 1 / k, 1 / k]])
+        self.blur_kernel.state_dict()['bias'].zero_()
+        # Sobel_filter
+        self.sobel_kernel = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.sobel_kernel.state_dict()['weight'][0] = torch.FloatTensor([[1, 0, -1],
+                                                                         [2, 0, -2],
+                                                                         [1, 0, -1]])
+        self.sobel_kernel.state_dict()['bias'].zero_()
+        # Sobel_filter2
+        self.sobel_kernel2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.sobel_kernel2.state_dict()['weight'][0] = torch.FloatTensor([[1, 2, 1],
+                                                                          [0, 0, 0],
+                                                                          [-1, -2, -1]])
+        self.sobel_kernel2.state_dict()['bias'].zero_()
+        # 鮮鋭化
+        k = 0.3
+        self.shape_kernel = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.shape_kernel.state_dict()['weight'][0] = torch.FloatTensor([[-k, -k, -k],
+                                                                         [-k, 1 + (8 * k), -k],
+                                                                         [-k, -k, -k]])
+        self.shape_kernel.state_dict()['bias'].zero_()
+
+    def forward(self, x):
+        # ここをコメントアウトすることで行いたい処理のみを行うことができる
+        x = self.blur_kernel(x)
+        x = self.sobel_kernel(x)
+        x = self.sobel_kernel2(x)
+        x = self.shape_kernel(x)
+
+        return x
+
+
+myfilter = MyFilter()
+y = myfilter(x)
+
+imshow(y)  # 描画
+
+
+# 自作カーネル
+class MyNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Blur_filter
+        k = 9
+        self.blur_kernel = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.blur_kernel.state_dict()['weight'][0] = torch.FloatTensor([[1 / k, 1 / k, 1 / k],
+                                                                        [1 / k, 1 / k, 1 / k],
+                                                                        [1 / k, 1 / k, 1 / k]])
+        self.blur_kernel.state_dict()['bias'].zero_()
+        # Sobel_filter
+        self.sobel_kernel = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.sobel_kernel.state_dict()['weight'][0] = torch.FloatTensor([[1, 0, -1],
+                                                                         [2, 0, -2],
+                                                                         [1, 0, -1]])
+        self.sobel_kernel.state_dict()['bias'].zero_()
+        # CNN
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 53 * 53, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.blur_kernel(x)
+        x = self.sobel_kernel(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 53 * 53)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
